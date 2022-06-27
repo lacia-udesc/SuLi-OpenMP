04afa561fdc551d927a670eab1f4867617bcc0d0
Pedro Miguel De Andrade Filho <108071812+brunafernandasoares@users.noreply.github.com>
2022-06-27 17:24:35 -0300

    Alteração entradas

--- SuLi-IC/0_modules.f90
+++ SuLi-IC/0_modules.f90
@@ -4,29 +4,28 @@ module disc
 real(8),parameter ::  pi = acos(-1.) 
 
 !Discretizações espaciais em x e y (metros), discretização temporal (segundos)
-real(8),parameter :: dx = 0.02, dy = 0.02, dz = 0.02
+real(8),parameter :: dx = 0.02, dy = 0.2, dz = 0.02
 
 real(8) :: t, dt = 0.001, t_i, t_a
 !Número de células para x, y e z (-); número de pontos para x, y e z (-); tempo de simulação (segundos)
 
 !Número de tempo por arquivo plotado
-real(8),parameter :: dt_frame = 1.
+real(8),parameter :: dt_frame = 0.001*10
 
-integer,parameter :: nx=int(.5/dx) , ny=int(1./dy), nz=int(0.9/dz)
+integer,parameter :: nx=int(.3/dx) , ny=int(1./dy), nz=int(1./dz)
 !nz=int(10./dz1-0.1+0.5) porque a última célula é maior (0.5)
-integer,parameter :: nx1=nx+1, ny1=ny+1, nz1=nz+1, ts = ceiling(20./0.001)
+integer,parameter :: nx1=nx+1, ny1=ny+1, nz1=nz+1, ts = ceiling(10./0.001)
 
 !Para fazer dz variável no espaço inicialmente criar uma função ...
 
+real(8),parameter :: uinicial = 0.0
 
-real(8),parameter :: uinicial = 0.31
+integer,parameter :: t_plot = 0 ! 0 = modo simples (velocidade, Level Set e IBM), 1 = modo completo (pressão, vorticidade, viscosidade)
 
-integer,parameter :: t_plot = 1 ! 0 = modo simples (velocidade, Level Set e IBM), 1 = modo completo (pressão, vorticidade, viscosidade)
-
-integer,parameter :: t_tempo = 1 ! 0 = Euler Explícito, 1 = RK 2, 2 = RK 3, 3 = AB2
-
+integer,parameter :: t_tempo = 2 ! 0 = Euler Explícito, 1 = RK 2, 2 = RK 3, 3 = AB2
 
 integer,parameter :: der = 1 ! 1 = upwind, 2 = centrado, 3 = upwind 2nd order (centrado só para advectivo clássico)
+
 integer,parameter :: adv_type = 1 ! 1 = advectivo clássico, 2 = rotacional, 3 = antissimétrico
 	
 integer,parameter :: obst_t = 0 ! 0 = sem obst, 1 = dunas, 2 = dunas2, 3 = gaussiano3D, 4 = beji, 5 = delft degrau, 6 = delft 1_2, 7 = SBRH calombos e buracos, 8 = fennema1990, 9 = aureli2008, bd_koshizuka1995eKleefsman2005
@@ -53,8 +52,8 @@ end module restart
 !Condicoes de contorno
 module cond 
 
-integer,parameter :: ccx0=0 !condicao de contorno parede x=0 --> 0 é periodico, 1 é free-slip, 2 é no-slip, 3 é prescrita, 4 é fluxo validacao
-integer,parameter :: ccxf=0 !condicao de contorno parede x=xf --> 0 é periodico, 1 é free-slip, 2 é no-slip, 3 é prescrita, 4 é saida livre
+integer,parameter :: ccx0=2 !condicao de contorno parede x=0 --> 0 é periodico, 1 é free-slip, 2 é no-slip, 3 é prescrita, 4 é fluxo validacao
+integer,parameter :: ccxf=2 !condicao de contorno parede x=xf --> 0 é periodico, 1 é free-slip, 2 é no-slip, 3 é prescrita, 4 é saida livre
 
 !Só pode usar condição periódica no final quando usar no começo e vice-versa
 
